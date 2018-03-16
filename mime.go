6a02e34043fad05c2491dd1e7e0156e3644273d9
pojozhang <pojo.zhang@gmail.com>
2018-03-16 14:32:47 +0800

    rename mime types

--- mime.go
+++ mime.go
@@ -3,7 +3,9 @@ package sugar
 const (
 	ContentType          = "Content-Type"
 	ContentTypeForm      = "application/x-www-form-urlencoded"
-	ContentTypeJson      = "application/json; charset=UTF-8"
-	ContentTypeXml       = "application/xml; charset=UTF-8"
+	ContentTypeJson      = "application/json"
+	ContentTypeJsonUtf8  = "application/json; charset=UTF-8"
+	ContentTypeXml       = "application/xml"
+	ContentTypeXmlUtf8   = "application/xml; charset=UTF-8"
 	ContentTypePlainText = "text/plain"
 )
