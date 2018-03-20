dd5d2f785dadd5525162d5c4011e84052fc19c18
pojozhang <pojo.zhang@gmail.com>
2018-03-21 00:58:57 +0800

    add tests

--- decoder_test.go
+++ decoder_test.go
@@ -1,11 +1,42 @@
 package sugar
 
 import (
+	"errors"
 	"github.com/stretchr/testify/assert"
+	"net/http"
 	"testing"
 )
 
 func TestDecoderChain_Next_Returns_Error_If_No_Decoders(t *testing.T) {
 	c := &DecoderChain{}
 	assert.Equal(t, DecoderNotFound, c.Next())
+}
+
+func TestJsonDecoder_Decode_Returns_Error_If_Fail_To_Read_Body(t *testing.T) {
+	context := &ResponseContext{Response: &http.Response{Header: http.Header{ContentType: []string{ContentTypeJson}}, Body: errorBody{}}}
+	decoder := &JsonDecoder{}
+	assert.NotNil(t, decoder.Decode(context, nil))
+}
+
+func TestXmlDecoder_Decode_Returns_Error_If_Fail_To_Read_Body(t *testing.T) {
+	context := &ResponseContext{Response: &http.Response{Header: http.Header{ContentType: []string{ContentTypeXml}}, Body: errorBody{}}}
+	decoder := &XmlDecoder{}
+	assert.NotNil(t, decoder.Decode(context, nil))
+}
+
+func TestPlainTextDecoder_Decode_Returns_Error_If_Fail_To_Read_Body(t *testing.T) {
+	context := &ResponseContext{Response: &http.Response{Header: http.Header{ContentType: []string{ContentTypePlainText}}, Body: errorBody{}}}
+	decoder := &PlainTextDecoder{}
+	assert.NotNil(t, decoder.Decode(context, nil))
+}
+
+type errorBody struct {
+}
+
+func (b errorBody) Read(p []byte) (n int, err error) {
+	return 0, errors.New("error")
+}
+
+func (b errorBody) Close() error {
+	return nil
 }
