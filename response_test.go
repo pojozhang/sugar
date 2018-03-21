457e20713cc8fa1cb48ea3252df2cbb46f162b48
pojozhang <pojo.zhang@gmail.com>
2018-03-21 15:39:26 +0800

    add a test

--- response_test.go
+++ response_test.go
@@ -2,18 +2,33 @@ package sugar
 
 import (
 	"github.com/stretchr/testify/assert"
+	"io/errors"
 	"testing"
 )
 
-func TestResponse_Read_When_Response_Has_Error(t *testing.T) {
+func TestResponse_Read_Returns_Error_When_Response_Has_Error(t *testing.T) {
 	var v string
 	resp := &Response{Error: EncoderNotFound}
 	_, err := resp.Read(&v)
 	assert.NotNil(t, err)
 }
 
-func TestResponse_ReadBytes_When_Response_Has_Error(t *testing.T) {
+func TestResponse_ReadBytes_Returns_Error_When_Response_Has_Error(t *testing.T) {
 	resp := &Response{Error: EncoderNotFound}
 	_, _, err := resp.ReadBytes()
 	assert.NotNil(t, err)
+}
+
+func TestResponse_Read_Returns_Error_If_DecoderChain_Returns_Error(t *testing.T) {
+	resp := &Response{decoders: []Decoder{&errorDecoder{}}}
+	var v string
+	_, err := resp.Read(&v)
+	assert.NotNil(t, err)
+}
+
+type errorDecoder struct {
+}
+
+func (d *errorDecoder) Decode(context *ResponseContext, chain *DecoderChain) error {
+	return errors.New("error")
 }
