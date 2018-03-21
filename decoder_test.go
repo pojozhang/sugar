68d9ec5a3e92f09a5d8bb396b49e502c0cc09524
pojozhang <pojo.zhang@gmail.com>
2018-03-21 15:28:29 +0800

    add tests

--- decoder_test.go
+++ decoder_test.go
@@ -3,6 +3,7 @@ package sugar
 import (
 	"errors"
 	"github.com/stretchr/testify/assert"
+	"io"
 	"net/http"
 	"testing"
 )
@@ -13,30 +14,53 @@ func TestDecoderChain_Next_Returns_Error_If_No_Decoders(t *testing.T) {
 }
 
 func TestJsonDecoder_Decode_Returns_Error_If_Fail_To_Read_Body(t *testing.T) {
-	context := &ResponseContext{Response: &http.Response{Header: http.Header{ContentType: []string{ContentTypeJson}}, Body: errorBody{}}}
+	context := &ResponseContext{Response: &http.Response{Header: http.Header{ContentType: []string{ContentTypeJson}}, Body: readErrorBody{}}}
+	decoder := &JsonDecoder{}
+	assert.NotNil(t, decoder.Decode(context, nil))
+}
+
+func TestJsonDecoder_Decode_Returns_Error_If_Fail_To_Unmarshal(t *testing.T) {
+	context := &ResponseContext{Response: &http.Response{Header: http.Header{ContentType: []string{ContentTypeJson}}, Body: unmarshalErrorBody{}}}
 	decoder := &JsonDecoder{}
 	assert.NotNil(t, decoder.Decode(context, nil))
 }
 
 func TestXmlDecoder_Decode_Returns_Error_If_Fail_To_Read_Body(t *testing.T) {
-	context := &ResponseContext{Response: &http.Response{Header: http.Header{ContentType: []string{ContentTypeXml}}, Body: errorBody{}}}
+	context := &ResponseContext{Response: &http.Response{Header: http.Header{ContentType: []string{ContentTypeXml}}, Body: readErrorBody{}}}
+	decoder := &XmlDecoder{}
+	assert.NotNil(t, decoder.Decode(context, nil))
+}
+
+func TestXmlDecoder_Decode_Returns_Error_If_Fail_To_Unmarshal(t *testing.T) {
+	context := &ResponseContext{Response: &http.Response{Header: http.Header{ContentType: []string{ContentTypeXml}}, Body: unmarshalErrorBody{}}}
 	decoder := &XmlDecoder{}
 	assert.NotNil(t, decoder.Decode(context, nil))
 }
 
 func TestPlainTextDecoder_Decode_Returns_Error_If_Fail_To_Read_Body(t *testing.T) {
-	context := &ResponseContext{Response: &http.Response{Header: http.Header{ContentType: []string{ContentTypePlainText}}, Body: errorBody{}}}
+	context := &ResponseContext{Response: &http.Response{Header: http.Header{ContentType: []string{ContentTypePlainText}}, Body: readErrorBody{}}}
 	decoder := &PlainTextDecoder{}
 	assert.NotNil(t, decoder.Decode(context, nil))
 }
 
-type errorBody struct {
+type readErrorBody struct {
 }
 
-func (b errorBody) Read(p []byte) (n int, err error) {
+func (b readErrorBody) Read(p []byte) (n int, err error) {
 	return 0, errors.New("error")
 }
 
-func (b errorBody) Close() error {
+func (b readErrorBody) Close() error {
+	return nil
+}
+
+type unmarshalErrorBody struct {
+}
+
+func (b unmarshalErrorBody) Read(p []byte) (n int, err error) {
+	return 0, io.EOF
+}
+
+func (b unmarshalErrorBody) Close() error {
 	return nil
 }
