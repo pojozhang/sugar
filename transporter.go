36e5c2ebdf1d9665f1202e405ff73a504a878630
pojozhang <pojo.zhang@gmail.com>
2018-05-02 15:21:20 +0800

    add comment

--- transporter.go
+++ transporter.go
@@ -2,6 +2,8 @@ package sugar
 
 import "net/http"
 
+// Transporter is an important interface for different http clients.
 type Transporter interface {
+	// Do issues the request.
 	Do(req *http.Request) (*http.Response, error)
 }
