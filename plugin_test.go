9cf5102e4fb6d8ec761bb57c638678fa4af17295
pojozhang <pojo.zhang@gmail.com>
2018-06-04 17:58:46 +0800

    update test

--- plugin_test.go
+++ plugin_test.go
@@ -1,6 +1,7 @@
 package sugar
 
 import (
+	"github.com/stretchr/testify/assert"
 	"net"
 	"net/http"
 	"testing"
@@ -8,17 +9,24 @@ import (
 )
 
 type mockTransporter struct {
+	count int
 }
 
 func (t *mockTransporter) Do(req *http.Request) (*http.Response, error) {
+	t.count++
 	return nil, &net.OpError{}
 }
 
-func TestRetryer(t *testing.T) {
-	p := PluginFunc(Retryer(3, time.Duration(1)*time.Second, 1.5, time.Duration(3)*time.Second))
+func TestRetryerShouldRetryIfTransporterReturnsAnErrorOfNetError(t *testing.T) {
+	const attempts = 3
+	p := PluginFunc(Retryer(attempts, time.Duration(1)*time.Second, 1.5, time.Duration(3)*time.Second))
+	m := &mockTransporter{}
 	c := Context{
-		transporter: &mockTransporter{},
+		transporter: m,
 		plugins:     []Plugin{p},
 	}
+
 	c.Next()
+
+	assert.Equal(t, attempts, m.count)
 }
