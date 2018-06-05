f8e11ed52d65343a0f4c477795748476f09142dd
pojozhang <pojo.zhang@gmail.com>
2018-06-05 23:49:00 +0800

    add test

--- decoder_test.go
+++ decoder_test.go
@@ -10,30 +10,35 @@ import (
 
 func TestDecoderChain_Next_Returns_Error_If_No_Decoders(t *testing.T) {
 	c := &DecoderChain{}
+
 	assert.Equal(t, DecoderNotFound, c.Next())
 }
 
 func TestJsonDecoder_Decode_Returns_Error_If_Fail_To_Read_Body(t *testing.T) {
 	context := &ResponseContext{Response: &http.Response{Header: http.Header{ContentType: []string{ContentTypeJson}}, Body: readErrorBody{}}}
 	decoder := &JsonDecoder{}
+
 	assert.NotNil(t, decoder.Decode(context, nil))
 }
 
 func TestJsonDecoder_Decode_Returns_Error_If_Fail_To_Unmarshal(t *testing.T) {
 	context := &ResponseContext{Response: &http.Response{Header: http.Header{ContentType: []string{ContentTypeJson}}, Body: unmarshalErrorBody{}}}
 	decoder := &JsonDecoder{}
+
 	assert.NotNil(t, decoder.Decode(context, nil))
 }
 
 func TestXmlDecoder_Decode_Returns_Error_If_Fail_To_Read_Body(t *testing.T) {
 	context := &ResponseContext{Response: &http.Response{Header: http.Header{ContentType: []string{ContentTypeXml}}, Body: readErrorBody{}}}
 	decoder := &XmlDecoder{}
+
 	assert.NotNil(t, decoder.Decode(context, nil))
 }
 
 func TestXmlDecoder_Decode_Returns_Error_If_Fail_To_Unmarshal(t *testing.T) {
 	context := &ResponseContext{Response: &http.Response{Header: http.Header{ContentType: []string{ContentTypeXml}}, Body: unmarshalErrorBody{}}}
 	decoder := &XmlDecoder{}
+
 	assert.NotNil(t, decoder.Decode(context, nil))
 }
 
@@ -41,6 +46,7 @@ func TestPlainTextDecoder_Decode_Returns_Error_If_Fail_To_Read_Body(t *testing.T
 	var out string
 	context := &ResponseContext{Response: &http.Response{Header: http.Header{ContentType: []string{ContentTypePlainText}}, Body: readErrorBody{}}, Out: &out}
 	decoder := &PlainTextDecoder{}
+
 	assert.NotNil(t, decoder.Decode(context, nil))
 }
 
@@ -58,7 +64,9 @@ func TestPlainTextDecoder_Will_Propagate_If_Out_Is_Not_Type_Of_String_Pointer(t 
 	context := &ResponseContext{Response: &http.Response{Header: http.Header{ContentType: []string{ContentTypePlainText}}, Body: readErrorBody{}}, Out: &out}
 	decoder := &PlainTextDecoder{}
 	nextDecoder := &mockDecoder{Called: false}
+
 	decoder.Decode(context, NewDecoderChain(&ResponseContext{Response: nil, Out: nil}, nextDecoder))
+
 	assert.True(t, nextDecoder.Called)
 }
 
@@ -66,6 +74,7 @@ func TestFileDecoder_Decode_Returns_Error_If_Out_Is_Not_Ptr_Of_OsFile(t *testing
 	context := &ResponseContext{Out: "string"}
 	chain := &DecoderChain{}
 	decoder := &FileDecoder{}
+
 	assert.Equal(t, DecoderNotFound, decoder.Decode(context, chain))
 }
 
@@ -89,4 +98,13 @@ func (b unmarshalErrorBody) Read(p []byte) (n int, err error) {
 
 func (b unmarshalErrorBody) Close() error {
 	return nil
+}
+
+func TestPlainTextDecoder_Decode_Returns_Error_If_Content_Type_Is_Wrong(t *testing.T) {
+	s := new(string)
+	context := &ResponseContext{Out: s, Response: &http.Response{Header: http.Header{ContentType: []string{ContentTypeForm}}}}
+	chain := &DecoderChain{}
+	decoder := &PlainTextDecoder{}
+
+	assert.Equal(t, DecoderNotFound, decoder.Decode(context, chain))
 }
